408fdc24ee1f42bbf521c39fe268e648ad848caf
vatsal <rajvatsal2004@gmail.com>
2025-04-28 14:14:35 +0530

    refactor(lastfm-api.ts): add try catch blocks

--- src/core/lastfm-api.ts
+++ src/core/lastfm-api.ts
@@ -5,10 +5,16 @@ interface GetUrlParams {
   [key: string]: string
 }
 
+// TODO: Add more specific type checking
+// Ex type tagName = 'Disco' | 'Rock' ...
 async function getAlbums(tagName: string) {
   const finalUrl = `${rootUrl}?api_key=${key}&format=json&method=tag.getTopAlbums&tag=${tagName}`
-  const response = await fetch(finalUrl, { method: 'GET', })
-  return response.json()
+  try {
+    const response = await fetch(finalUrl, { method: 'GET' })
+    return response.json()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 function getUrl(queries: GetUrlParams) {
